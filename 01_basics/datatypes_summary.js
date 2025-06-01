9d33b3e4c990337b0af589170e5ec48d3ed2ec16
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2025-06-01 21:36:05 +0000

    datatyoes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 categories/types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score =100
+const scoreValue = 1000.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId= Symbol('123');
+console.log(id==anotherId);
+
+
+// const bigNumber = 3456543576654356754n
+
+//Reference Type ( Non Primitive type)
+
+// array , Objects, functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
