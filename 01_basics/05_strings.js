94a6b1cfaa0a2860c49af41f2b0ce7e7c7daa808
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2025-06-03 15:10:12 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+const gamename=new String('hiteshhc');
+
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
