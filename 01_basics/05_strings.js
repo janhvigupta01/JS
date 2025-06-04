ac2ffe496c267ff3d830828bc1cf35cb6f7fb0f5
JANHVI GUPTA <150956981+janhvigupta01@users.noreply.github.com>
2025-06-04 15:57:11 +0000

    01_basics complete

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,3 +8,27 @@ const gamename=new String('hiteshhc');
 
 console.log(gamename[0]);
 console.log(gamename.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
